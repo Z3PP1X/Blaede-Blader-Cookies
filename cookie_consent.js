3ed642f7126282735cf2848ddbb19935c3165bf9
Patrick Fuchs <p.fuchs@hm.edu>
2023-08-14 10:05:51 +0200

    added decline function

--- cookie_consent.js
+++ cookie_consent.js
@@ -1,13 +1,13 @@
 const cookieBanner = document.getElementById("cookieBanner");
 const acceptBtn = document.getElementById("acceptBtn");
+const declineBtn = document.getElementById("declineBtn");
+const cookieStatus = getCookie("cookieStatus");
 
-
-const hasAcceptedCookies = getCookie("cookieConsent");
-
-if (hasAcceptedCookies) {
+if (cookieStatus === "accepted") {
+    cookieBanner.style.display = "none"; 
+} else if (cookieStatus === "declined") {
     cookieBanner.style.display = "none"; 
 }
-
 
 function getCookie(name) {
     const cookies = document.cookie.split("; ");
@@ -20,22 +20,27 @@ function getCookie(name) {
     return null;
 }
 
-
 function setCookie(name, value, expirationDate) {
     const expires = expirationDate.toUTCString();
     document.cookie = `${name}=${encodeURIComponent(value)}; expires=${expires}; path=/`;
 }
 
-
 function acceptCookies() {
-    
     const twoMonthsFromNow = new Date();
     twoMonthsFromNow.setMonth(twoMonthsFromNow.getMonth() + 2);
 
-    setCookie("cookieConsent", "true", twoMonthsFromNow);
+    setCookie("cookieStatus", "accepted", twoMonthsFromNow);
 
     cookieBanner.style.display = "none";
+    trackAnalytics("cookie_accepted");
 }
 
+function declineCookies() {
+    setCookie("cookieStatus", "declined", new Date(0)); 
+
+    cookieBanner.style.display = "none";
+    trackAnalytics("cookie_declined");
+}
 
 acceptBtn.addEventListener("click", acceptCookies);
+declineBtn.addEventListener("click", declineCookies);
